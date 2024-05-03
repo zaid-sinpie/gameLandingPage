e29423781053c6a4f7a6d43da28222839377e88f
zaid-sinpie <zaidmasuldar@gmail.com>
2024-05-04 01:31:57 +0530

    adjustment hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,13 @@ const Hero = () => {
   return (
     <section className="hero-section flex justify-end items-end">
       <figure className="h-full flex flex-col justify-between items-end text-red-500 py-[5rem]">
-        <div className="px-[2rem] py-[1rem] bg-[#414141] text-[18px] font-bold text-[#d7ff55] rounded-l-full border-2 border-[#d7ff55] hover:text-[#414141] hover:bg-[#d7ff55] cursor-pointer">
+        <div className="px-[2rem] py-[1rem] bg-[#5b5b5bc5] backdrop-blur-sm text-[18px] font-bold text-[#d7ff55] rounded-l-full border-2 border-[#d7ff55] hover:text-[#414141] hover:bg-[#d7ff55a2] cursor-pointer">
           <button>
             <h2>DOWNLOAD <i className="fa-solid fa-download"></i></h2>
           </button>
         </div>
-        <div className="flex gap-2 px-[0.5rem] py-[0.5rem] text-[#414141] text-xl font-bold bg-[#d7ff55] rounded-md border-2 border-[#d7ff55] cursor-pointer">
+        <div></div>
+        <div className="flex gap-2 px-[0.5rem] py-[0.5rem] text-[#414141] text-xl font-bold bg-[#d7ff55a2] rounded-md border-2 border-[#d7ff55] backdrop-blur-sm cursor-pointer">
           <a href="#"><i className={`fa-brands fa-twitter ${cssClasses}`}></i></a>
           <a href="#"><i className={`fa-brands fa-youtube ${cssClasses}`}></i></a>
           <a href="#"><i className={`fa-brands fa-linkedin ${cssClasses}`}></i></a>
