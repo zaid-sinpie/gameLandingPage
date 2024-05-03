8a2ec9ce68e622e2f7ccb63ed5e4457554b916d1
zaid-sinpie <zaidmasuldar@gmail.com>
2024-05-03 18:58:48 +0530

    hero page done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 const Hero = () => {
+  const cssClasses = ` border-2 border-[#414141] p-1 rounded-md bg-[#414141] text-[#d7ff55] hover:bg-[#d7ff55] hover:text-[#414141]`;
   return (
     <section className="hero-section flex justify-end items-end">
-      <figure className="h-full flex flex-col justify-between items-center text-red-500 py-[5rem]">
-        <div className="px-[2rem] py-[1rem] bg-[#414141] text-xl font-bold text-[#d7ff55] rounded-l-full border-2 border-[#d7ff55] hover:text-[#414141] hover:bg-[#d7ff55] cursor-pointer">
+      <figure className="h-full flex flex-col justify-between items-end text-red-500 py-[5rem]">
+        <div className="px-[2rem] py-[1rem] bg-[#414141] text-[18px] font-bold text-[#d7ff55] rounded-l-full border-2 border-[#d7ff55] hover:text-[#414141] hover:bg-[#d7ff55] cursor-pointer">
           <button>
-            <h2>DOWNLOAD</h2>
+            <h2>DOWNLOAD <i className="fa-solid fa-download"></i></h2>
           </button>
         </div>
-        <div className="px-[2rem] py-[1rem] bg-[#414141] text-xl font-bold text-[#d7ff55] rounded-md border-2 border-[#d7ff55] hover:text-[#414141] hover:bg-[#d7ff55]">
-          <button>
-            <h2>SHARE</h2>
-          </button>
+        <div className="flex gap-2 px-[0.5rem] py-[0.5rem] text-[#414141] text-xl font-bold bg-[#d7ff55] rounded-md border-2 border-[#d7ff55] cursor-pointer">
+          <a href="#"><i className={`fa-brands fa-twitter ${cssClasses}`}></i></a>
+          <a href="#"><i className={`fa-brands fa-youtube ${cssClasses}`}></i></a>
+          <a href="#"><i className={`fa-brands fa-linkedin ${cssClasses}`}></i></a>
+          <a href="#"><i className={`fa-brands fa-square-instagram ${cssClasses}`}></i></a>
         </div>
       </figure>
     </section>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,5 @@
 
-const Section = ({children}) => {
+const Section = ({children, }) => {
   return (
     <section className='wrapper-section relative w-screen h-screen bg-gray-700 overflow-x-auto overflow-y-hidden flex scroll-smooth'>
       {children}
