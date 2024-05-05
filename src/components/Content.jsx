e3955a78acde5901b08a44742a9f3c90116c6ce0
zaid-sinpie <zaidmasuldar@gmail.com>
2024-05-05 18:55:40 +0530

    content text added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ImgSlider from './ImgSlider.jsx'
+import React from "react";
+import ImgSlider from "./ImgSlider.jsx";
+import ContentText from "./ContentText.jsx";
+
 
 const Content = () => {
   return (
-    <section className='min-h-[100vh] min-w-[100vw] bg-[#414141] relative flex justify-around items-start pt-8'>
-      <ImgSlider/>
+    <section className="min-h-[100vh] min-w-[100vw] bg-[#414141] relative flex flex-col justify-around max-sm:justify-start items-start pt-8 max-sm:pt-[7rem] max-sm:gap-4">
+      <ImgSlider />
+      <ContentText />
     </section>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
---
+++ src/components/ContentText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContentText = () => {
+    
+  return (
+    <div className='ml-[10rem] max-sm:ml-0'>
+        <h1 className='text-6xl font-extrabold text-[#696969] max-sm:ml-4'>WAIFU'S WAR ZONE</h1>
+    </div>
+  )
+}
+
+export default ContentText
