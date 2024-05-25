304b4919c683ee2153e5c660e737548447147528
zaid-sinpie <zaidmasuldar@gmail.com>
2024-05-26 01:02:57 +0530

    clock tick rotaion animation

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -9,14 +9,14 @@ const Content = () => {
       <div className="flex max-sm:flex-col justify-between items-start gap-[5rem] max-sm:gap-8 max-sm:items-center z-10">
         <ImgSlider />
         <div className="flex flex-col">
-          <button className="text-4xl font-bold text-[#727272] bg-[#39393973] px-10 py-6 rounded-full border-8 border-[#727272] uppercase max-sm:text-2xl shadow-md backdrop-blur-sm">
+          <button className="text-4xl font-bold text-[#727272] hover:text-[#8f8f8f] bg-[#39393973] px-10 py-6 rounded-full border-8 border-[#727272] hover:border-[#8f8f8f] uppercase max-sm:text-2xl shadow-md backdrop-blur-sm">
             Download Now
           </button>
         </div>
         
       </div>
       <ContentText />
-      <img src={imgBg} alt="background spider image" className="absolute w-[100rem] -right-[20rem] opacity-5 pointer-events-none max-sm:-right-[5rem]"/>
+      <img src={imgBg} alt="background spider image" className="absolute w-[100rem] -right-[20rem] opacity-5 pointer-events-none max-sm:-right-[5rem] animate-tick"/>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ export default {
     extend: {
       fontFamily:{
         Lilita:["Lilita One","sans-serif"],
-      }
+      },
+      keyframes: {
+        tick: {
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        tick: 'tick 60s steps(60) infinite',
+      },
     },
   },
   plugins: [],
